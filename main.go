a598ea3d8c6e18c5c0ee2d06a54d0ae4734af3cd
premsynfosys <prem@synfosys.com>
2020-08-28 14:17:07 +0530

    test

--- main.go
+++ main.go
@@ -23,6 +23,7 @@ var err error
 
 // changed db config name and pwd
 func init() {
+	//abc
 	//	connection, err = DBdriver.ConnectSQL("mysql", "3306", "AMS", "Admin&123", "ams") //aws pwd //AMS is usrname
 	connection, err = DBdriver.ConnectSQL("mysql", "localhost:3306", "AMS", "Admin&123", "ams?parseTime=true")
 }
